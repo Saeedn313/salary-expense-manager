d532e7195995d9b412f1f0cb4aa30f5f2044907a
Saeed Nazari <nsaeid.2002@gmail.com>
2025-08-23 02:37:50 +0330

    making some changes
    1.add get all users function.
    2.add two new column to users table
    3.change delete function in db.py

--- models/db.py
+++ models/db.py
@@ -16,6 +16,8 @@ class UserDb:
                 family TEXT,
                 role TEXT,
                 hourly_rate INTEGER,
+                total_hour INTEGER,
+                total_minute INTEGER,
                 salary INTEGER
                 )
             """)
@@ -28,37 +30,33 @@ class UserDb:
         SELECT * FROM users
         """).fetchall()
             
-        all_users = []
-        for user in rows:
-            all_users.append(user)
-
-        return all_users
+        return rows
 
     def add(self, user):
         with sqlite3.connect(db_file) as conn:
             cursor = conn.cursor()
             cursor = conn.cursor()
             cursor.execute("""
-            INSERT INTO users (name, family, role, hourly_rate, salary)
-            VALUES (?, ?, ?, ?, ?)
-            """, (user.name, user.family, user.role, user.hourly_rate, user.salary))
+            INSERT INTO users (name, family, role, hourly_rate, total_hour, total_minute, salary)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (user.name, user.family, user.role, user.hourly_rate,user.total_hour, user.total_minute, user.salary))
             conn.commit()
             user.id = cursor.lastrowid
 
 
-    def delete(self, user):
+    def delete(self, user_id):
         with sqlite3.connect(db_file) as conn:
             cursor = conn.cursor()
             cursor.execute("""
         DELETE FROM users WHERE id=?
-        """, (user.id,))
+        """, (user_id,))
             
     def update(self, user):
         with sqlite3.connect(db_file) as conn:
             cursor = conn.cursor()
             cursor.execute("""
-            UPDATE uesrs SET name=?, family=?, role=?, hourly_rate=?, salary=? WHERE id=?
-            """, (user.name, user.family, user.role, user.hourly_rate, user.salary, user.id))
+            UPDATE uesrs SET name=?, family=?, role=?, hourly_rate=?,total_hour=?, total_minute=?, salary=? WHERE id=?
+            """, (user.name, user.family, user.role, user.hourly_rate,user.total_hour, user.total_minute, user.salary, user.id))
 
 
 
--- models/users.py
+++ models/users.py
@@ -9,6 +9,7 @@ class User:
         self.total_hour = total_hour
         self.total_minute = total_minute
         self.salary = self.calc_salary()
+        self.id = id
 
 
     def full_name(self):
@@ -19,11 +20,32 @@ class User:
         salary = int(total_hour * self.hourly_rate)
 
         return salary
+    
+    def get_all_users(self, rows):
+        all_users = []
+        for row in rows:
+            user_id, name, family, role, hourly_rate, total_hour, total_minute, _ = row
+            if role == 'Developer':
+                user = Developer(name, family, hourly_rate, total_hour, total_minute)
+            elif role == "Manager":
+                user = Manager(name, family, hourly_rate, total_hour, total_minute)
 
+            user.id = user_id
+            all_users.append(user)
+        
+        return all_users
+    
+    def __repr__(self):
+        return f"<({self.id}){self.name} - {self.family} - {self.hourly_rate} - {self.salary}>"
+    
+
+            
 class Developer(User):
     def __init__(self, name, family, hourly_rate, total_hour, total_minute):
         super().__init__(name, family, hourly_rate, total_hour, total_minute)
         self.role = 'Developer'
+
+    
 
 class Manager(User):
     def __init__(self, name, family, hourly_rate, total_hour, total_minute):
@@ -53,10 +75,16 @@ class Cost:
 # print(getattr(user, 'role'))
 # print(help(user))
 
-# userdb = UserDb()
-# userdb.init_db()
+userdb = UserDb()
 
-# dev1 = Developer('saeed', 'nazari', 2000000, 12, 43)
-# print(dev1)
-# userdb.add_user(dev1)
+# userdb.init_db()
+manager1 = Manager('hassan', 'mahdi', 500000, 10, 4)
+dev1 = Developer('saeed', 'nazari', 2000000, 12, 43)
+# userdb.delete(manager1)
+userdb.delete(1)
+rows = userdb.fetch_all()
+print(rows)
+# print(dev1.get_all_users(rows))
+
+
 
